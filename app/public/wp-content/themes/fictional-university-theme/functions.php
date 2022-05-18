b5c8c591663be76ca5cd444b29d76533d48ae315
Abd Bashir <101551397+Abd-Bashir@users.noreply.github.com>
2022-05-18 23:37:09 +0300

    Finished the professor dynamic added the images, custom fields for banner and subtitle

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -11,6 +11,10 @@ function university_files() {
 
  function university_features() {
    add_theme_support('title-tag');
+   add_theme_support('post-thumbnails');
+   add_image_size( 'professorLandscape', 400, 260, true );
+   add_image_size( 'professorPortrait', 480, 650, true );
+   add_image_size( 'pageBanner', 1500, 350, true );
    register_nav_menu('headerMenuLocation' , 'Header Menu Location');
    register_nav_menu('footerMenuLocation' , 'Footer Menu Location');
    register_nav_menu('exploreMenuLocation' , 'Explore Menu Location');
