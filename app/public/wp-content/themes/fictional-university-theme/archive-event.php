625f09d519a579150120d7379b8e29677267850a
Abd Bashir <101551397+Abd-Bashir@users.noreply.github.com>
2022-05-19 00:37:07 +0300

    Less Duplication Cleaned all duplicated code and created a function and template part to display some repeated content

--- app/public/wp-content/themes/fictional-university-theme/archive-event.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-event.php
@@ -1,14 +1,12 @@
 <?php get_header(); ?>
 
-<div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p><?php the_archive_description(); ?></p>
-        </div>
-      </div>
-    </div>
+<?php 
+  pageBanner(array(
+    'title' => 'All Events',
+    'subtitle' => 'See What Is Going On In Our World.'
+  )); 
+?>
+
 
     <div class="container container--narrow page-section">
       <?php 
@@ -16,25 +14,7 @@
       
         while(have_posts()) {
           the_post();
-        ?>
-
-        <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-              <span class="event-summary__month"><?php  
-                $eventDate = new DateTime(get_field('event_date'));
-                echo $eventDate -> format('M');
-              ?></span>
-              <span class="event-summary__day"><?php  
-                $eventDate = new DateTime(get_field('event_date'));
-                echo $eventDate -> format('d');
-              ?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-            </div>
-          </div>
-      <?php
+          get_template_part('template-parts/event');
         }
       ?>
 
